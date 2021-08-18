0c8c1ef2698b4ba573bec8d379e86c49b5e50736
Aleksandr Kryukov <kryukov86@mail.ru>
2021-08-18 20:37:26 +0700

    Initial Commit

---
+++ TwoViewConstrains/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwoViewConstrains
+//
+//  Created by Александр Крюков on 18.08.2021.
+//  Copyright © 2021 Александр Крюков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
